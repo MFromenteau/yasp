108777bcdedb3059709dfbee0be9477b10f158fe
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2018-01-12 10:59:56 +0100

    minireview videoCtrl

--- src/Controller/VideoCtrl.php
+++ src/Controller/VideoCtrl.php
@@ -11,26 +11,26 @@ class VideoCtrl extends Controller
 {
 	//$request = Request::createFromGlobals();
 	/**
-	 * @Route("/video/theme", name="theme")
+	 * @Route("/video/{id}/theme", name="getThemeByVideoId")
 	 * @Method({"GET"})
 	 */
-	public function getVideoByTheme(){
+	public function getThemeByVideoId(){
 
-			//www.yasp.fr/video/connecte/
+
 	}
 	/**
-	 * @Route("/{id}/video", name="recherche")
+	 * @Route("/video/{id}", name="getVideoById")
 	 * @Method({"GET"})
 	 */
 	public function getVideoById($id){
 
-			//www.yasp.fr/67890/video
+
 	}
 	/**
-	 * @Route("/video/upload/", name="upload")
+	 * @Route("/video/upload/", name="uploadVideo")
 	 * @Method({"POST"})
 	 */
-	public function upload(){
+	public function uploadVideo(){
 		$session = new Session();
 		$session->start();
 		$idUser = $session->get("user").id;
@@ -38,105 +38,74 @@ class VideoCtrl extends Controller
 		$titre = mysqli_real_escape_string($request->request->get('titre'));
 		$url = mysqli_real_escape_string($request->request->get('url'));
 
-			//www.yasp.fr/video/upload
+
 	}
 	/**
-	 * @Route("/video/upload/", name="uploadGET")
-	 * @Method({"GET"})
-	 */
-	public function uploadGet(){
-			//www.yasp.fr/video/upload
-	}
-	/**
-	 * @Route("/video/{id}/liste", name="liste")
+	 * @Route("/user/{id}/video/list", name="getlisteVideoByUserId")
 	 * @Method({"GET"})
 	 */
 	public function getAllVideoByIdUser($id){
 
-			//www.yasp.fr/video/67890/liste
+
 	}
 	/**
-	 * @Route("/video/{id_Video}/nouveau", name="nouveau")
+	 * @Route("/video/{id}/new/comment", name="newComment")
 	 * @Method({"POST"})
 	 */
-	public function createComment($id_Video){
+	public function createCommentByIdVideo($id){
 		$idUser = mysqli_real_escape_string($request->request->get('idUser'));
 		$text = mysqli_real_escape_string($request->request->get('text'));
 
-			//www.yasp.fr/video/67890/nouveau
+
 	}
 	/**
-	 * @Route("/video/{id_Video}/nouveau", name="nouveau")
-	 * @Method({"GET"})
-	 */
-	public function createCommentGet(){
-			//www.yasp.fr/video/67890/nouveau
-	}
-	
-	/**
-	 * @Route("/video/{id_Video}/supression", name="supression")
+	 * @Route("/video/{id}/delete/comment", name="deleteComment")
 	 * @Method({"POST"})
 	 */
-	public function deleteComment($id_Video){
+	public function deleteCommentByIdVideo($id){
 		$idComment = mysqli_real_escape_string($request->request->get('idComment'));
 
 			//www.yasp.fr/video/67890/supression
 	}
 	/**
-	 * @Route("/video/{id_Video}/supression", name="supression")
-	 * @Method({"GET"})
-	 */
-	public function deleteCommentGet($id_Video){
-			//www.yasp.fr/video/67890/supression
-	}
-	/**
-	 * @Route("/video/{id_Video}/edit", name="edit")
+	 * @Route("/video/{id}/edit/comment", name="editComment")
 	 * @Method({"POST"})
 	 */
-	public function modifyComment($id_Video){
+	public function editComment($id){
 		$idComment = mysqli_real_escape_string($request->request->get('idComment'));
 		$newComment = mysqli_real_escape_string($request->request->get('newComment'));
 
 			//www.yasp.fr/video/67890/edit
 	}
 	/**
-	 * @Route("/video/{id}/com_by_user", name="commentaire_by_user")
+	 * @Route("/user/{id}/comment/list", name="getAllCommentByUserId")
 	 * @Method({"GET"})
 	 */
-	public function GetCommentByUser($id){
+	public function getAllCommentByUserId($id){
 
-			//www.yasp.fr/video/67890/com_by_user
 	}
 	/**
-	 * @Route("/video/{$id_Video}/com_by_video", name="commentaire_by_video")
+	 * @Route("/video/{id}/comment/list", name="getAllCommentByVideoId")
 	 * @Method({"GET"})
 	 */
-	public function GetCommentByVid($id_Video){
+	public function getAllCommentByVideoId($id){
 
-			//www.yasp.fr/video/com_by_video/67890
 	}
 	/**
-	 * @Route("/video/com_by_id/{id}", name="commentaire_by_id")
+	 * @Route("/comment/{id}", name="getCommentById")
 	 * @Method({"GET"})
 	 */
-	public function GetCommentById($id){
+	public function getCommentById($id){
 
-			//www.yasp.fr/video/com_by_id/67890
+
 	}
 	/**
-	 * @Route("/video/{$id_Video}/achat", name="achat")
+	 * @Route("/video/{id}/buy", name="buyVideoById")
 	 * @Method({"POST"})
 	 */
-	public function acheter($id_Video){
+	public function buyVideoById($id){
 		$idUser = mysqli_real_escape_string($request->request->get('idUser'));
 
-			//www.yasp.fr/video/acheter/67890
-	}
-	/**
-	 * @Route("/video/{$id_Video}/achat", name="achat")
-	 * @Method({"GET"})
-	 */
-	public function acheterGet(){
 			//www.yasp.fr/video/acheter/67890
 	}
 }
